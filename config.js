4ea910210af74f048be08df4bb41d396d18f5276
BikiBoruah <55024852+BikiBoruah@users.noreply.github.com>
2026-02-11 15:23:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ const CONFIG = {
     colors: {
         backgroundStart: "#FFD8DF",      // Gradient start (try pastel colors for a soft look)
         backgroundEnd: "#FFAAB8",        // Gradient end (should complement backgroundStart)
-        buttonBackground: "#F0FFDF",     // Button color (should stand out against the background)
+        buttonBackground: "#A8DF8E",     // Button color (should stand out against the background)
         buttonHover: "#B15E6C",          // Button hover color (slightly lighter than buttonBackground)
         textColor: "#2D3047"             // Text color (make sure it's readable!)
     },
